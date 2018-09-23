d85fec4f3acb6f841c828dc2d1383b0645644c18
Ilia Okunev <iliaokunev@gmail.com>
2018-09-23 21:02:12 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-module.exports = function getZerosCount(number, base) {
-    var x = Number.MAX_VALUE;
-  var y = base;
-  for (var i = 2; i <= base; i++) {
-    if (y % i === 0) {
-      var p = 0;
-      while (y % i === 0) {
-         y = y / i;     
-         p++;
-      }
-      var a = 0;
-      var b = Math.floor(number / i);
-      while (b > 0) {
-        a += b;
-        b = Math.floor(b / i);
-      }
-      x = Math.min(x, Math.floor(a / p))
-    }
-  }
-  return x;
+module.exports = function getZerosCount(number) {
+  let count = 0;
+	while(number>= 5){
+		number = Math.floor(number / 5);
+    count = count + number;
+	}
+  return count;
 }
